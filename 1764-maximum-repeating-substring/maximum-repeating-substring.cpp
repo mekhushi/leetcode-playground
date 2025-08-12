3df2c7d67118e6bf9ff88dad25ff31f1fe61753b
Khushi singh <145558409+mekhushi@users.noreply.github.com>
2025-08-12 16:05:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (29.46%) - LeetSync

---
+++ 1764-maximum-repeating-substring/maximum-repeating-substring.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxRepeating(string sequence, string word) {
+        int count = 0;
+        string temp = word;
+        while (sequence.find(temp) != string::npos) {
+            temp += word;
+            count++;
+        }
+        return count;
+    }
+};
