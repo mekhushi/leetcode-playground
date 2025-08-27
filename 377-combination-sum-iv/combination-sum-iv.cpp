444279db1f9278307a51c51ceb5ba240c8e01145
Khushi singh <145558409+mekhushi@users.noreply.github.com>
2025-08-27 11:02:58 +0530

    Time: 3 ms (39.29%) | Memory: 9.1 MB (88.05%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -1,14 +1,18 @@
 class Solution {
  public:
   int combinationSum4(vector<int>& nums, int target) {
-    vector<unsigned long> dp(target + 1);
-    dp[0] = 1;
+    vector<int> memo(target + 1, -1);
+    memo[0] = 1;
+    return dfs(nums, target, memo);
+  }
 
-    for (int i = 1; i <= target; ++i)
-      for (const int num : nums)
-        if (i >= num)
-          dp[i] += dp[i - num];
-
-    return dp[target];
+ private:
+  int dfs(vector<int>& nums, int target, vector<int>& memo) {
+    if (memo[target] != -1) return memo[target];
+    long res = 0;
+    for (int num : nums)
+      if (target >= num)
+        res += dfs(nums, target - num, memo);
+    return memo[target] = (int)res;
   }
 };
