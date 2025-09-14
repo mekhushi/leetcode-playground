66cbc404aa749e8c940a8b9a6d482790fdfb821c
Khushi singh <145558409+mekhushi@users.noreply.github.com>
2025-09-14 09:10:25 +0530

    Time: 64 ms (16.46%) | Memory: 46.8 MB (9.49%) - LeetSync

---
+++ 1006-vowel-spellchecker/vowel-spellchecker.cpp
@@ -0,0 +1,30 @@
+class Solution {
+ public:
+  vector<string> spellchecker(vector<string>& wordlist, vector<string>& queries) {
+    unordered_map<string, string> dict;
+    for (auto& w : wordlist) {
+      dict.emplace(w, w);
+      dict.emplace("$" + lower(w), w);
+      dict.emplace(vowelKey(w), w);
+    }
+
+    vector<string> ans;
+    for (auto& q : queries) {
+      string l = "$" + lower(q), v = vowelKey(q);
+      if (dict.count(q)) ans.push_back(dict[q]);
+      else if (dict.count(l)) ans.push_back(dict[l]);
+      else if (dict.count(v)) ans.push_back(dict[v]);
+      else ans.push_back("");
+    }
+    return ans;
+  }
+
+ private:
+  string lower(const string& s) {
+    string t; for (char c : s) t += tolower(c); return t;
+  }
+  string vowelKey(const string& s) {
+    string t; for (char c : s) t += isVowel(c) ? '*' : tolower(c); return t;
+  }
+  bool isVowel(char c) { return string("aeiouAEIOU").find(c) != string::npos; }
+};
